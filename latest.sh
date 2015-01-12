6175f5bdb801bc5cbb07b1875a312e0cacc58fd6
Paul Mason <pdm126@hotmail.co.uk>
2015-01-12 22:24:56 +0000

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -1,7 +1,11 @@
 #! /bin/bash                                                                                                                                                            
-
-sudo apt-get install emacs nbtscan terminator netcat whois aircrack-ng virtualbox git traceroute mtr nmap wine ngrep wireshark tshark hydra john tcpdump
-sudo apt-get install reaver chkrootkit curl python onesixtyone ike-scan unrar bluefish bless sslscan -y
+# install basic useful stuff
+# packages accepted by -y
+sudo apt-get install emacs nbtscan terminator netcat whois aircrack-ng git traceroute mtr nmap wine ngrep wireshark tshark hydra john tcpdump
+sudo apt-get install reaver chkrootkit curl python onesixtyone ike-scan unrar bluefish bless sslscan dnsutils -y
+#get virtualbox may break dkms on dist upgrade
+sudo apt-get virtualbox
+# packages wich require interaction
 sudo apt-get install rdesktop nikto
 sudo apt-get install clamav rkhunter
 sudo apt-get install kismet
@@ -10,6 +14,8 @@ sudo apt-get install powertop
 sudo apt-get install ettercap-text-only ettercap-graphical
 
 # sudo uwf enable ??
+
+# make folder framework and wget useful scripts 
 cd Dcouments
 mkdir pentest
 cd pentest
@@ -22,4 +28,6 @@ wget -nc http://dl.google.com/android/android-sdk_r22.6-linux.tgz
 tar -xvf android-sdk_r22.6-linux.tgz
 rm -rf android-sdk_r22.6-linux.tgz
 cd ..
-
+cd tools
+svn checkout http://theharvester.googlecode.com/svn/trunk/ theharvester
+chmod 777 theHarvester.py
