6e6cc89f54458406974cb7400a317368a4b56282
Paul Mason <pdm126@hotmail.co.uk>
2015-01-12 22:37:06 +0000

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -1,11 +1,11 @@
-#! /bin/bash                                                                                                                                                            
-# install basic useful stuff
-# packages accepted by -y
+#! /bin/bash                                                                                                        
+# install basic useful stuff                                                                                                                                            
+# packages accepted by -y                                                                                                          
 sudo apt-get install emacs nbtscan terminator netcat whois aircrack-ng git traceroute mtr nmap wine ngrep wireshark tshark hydra john tcpdump
 sudo apt-get install reaver chkrootkit curl python onesixtyone ike-scan unrar bluefish bless sslscan dnsutils -y
-#get virtualbox may break dkms on dist upgrade
+#get virtualbox may break dkms on dist upgrade                                                                                                                          
 sudo apt-get virtualbox
-# packages wich require interaction
+# packages wich require interaction                                                                                                                                     
 sudo apt-get install rdesktop nikto
 sudo apt-get install clamav rkhunter
 sudo apt-get install kismet
@@ -13,10 +13,10 @@ sudo apt-get install ntop
 sudo apt-get install powertop
 sudo apt-get install ettercap-text-only ettercap-graphical
 
-# sudo uwf enable ??
+# sudo uwf enable ??                                                                                                                                                    
 
-# make folder framework and wget useful scripts 
-cd Dcouments
+# make folder framework and wget useful scripts                                                                                                                         
+cd ~/Documents
 mkdir pentest
 cd pentest
 mkdir explots
@@ -30,4 +30,5 @@ rm -rf android-sdk_r22.6-linux.tgz
 cd ..
 cd tools
 svn checkout http://theharvester.googlecode.com/svn/trunk/ theharvester
+cd theharvester
 chmod 777 theHarvester.py
