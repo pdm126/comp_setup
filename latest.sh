c169ce48d58f2171e5c034d0e1645ed7704fcc52
Paul Mason <pdm126@hotmail.co.uk>
2015-01-12 21:55:38 +0000

    Create latest.sh

---
+++ latest.sh
@@ -0,0 +1,8 @@
+#! /bin/bash                                                                                                                                                            
+
+sudo apt-get install emacs nbtscan terminator netcat whois aircrack-ng virtualbox git traceroute mtr nmap wine ngrep wireshark tshark hydra john tcpdump
+sudo apt-get install reaver chkrootkit curl python onesixtyone ike-scan unrar bluefish bless -y
+sudo apt-get install rdesktop nikto
+sudo apt-get install kismet
+sudo apt-get install ntop
+sudo apt-get install ettercap-text-only ettercap-graphical
