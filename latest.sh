38571a8efb173573ee60898d4e9237e93f8ff7eb
Paul Mason <pdm126@hotmail.co.uk>
2015-01-12 22:07:45 +0000

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -3,6 +3,7 @@
 sudo apt-get install emacs nbtscan terminator netcat whois aircrack-ng virtualbox git traceroute mtr nmap wine ngrep wireshark tshark hydra john tcpdump
 sudo apt-get install reaver chkrootkit curl python onesixtyone ike-scan unrar bluefish bless sslscan -y
 sudo apt-get install rdesktop nikto
+sudo apt-get install clamav rkhunter
 sudo apt-get install kismet
 sudo apt-get install ntop
 sudo apt-get install powertop
